0544ce2f3925050f1a1b03cba3bd22e6e840f0d8
ririnkmlsr <ririnkmlsr28@gmail.com>
2019-08-01 17:02:37 +0700

    soal jambu

---
+++ soal1.php
@@ -0,0 +1,13 @@
+<?php
+   $hj = 15000;
+   $bd = 6;
+   $pd = 5;
+   $hd = 2000;
+   $uj = ($bd*$pd)*$hj;
+   $ud = $hj*$hd;
+   $hasil = $uj+$ud;
+   echo "<br>banyak  jambu : Rp.".$hj;
+   echo "<br>hasil penjualan dus".$ud;
+   echo "<br>hasil penjualan jambu".$uj;
+   echo "<br>uang hasil penjualan adalah ".$hasil;
+   ?>
